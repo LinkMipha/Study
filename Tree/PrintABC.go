1c7742b9ba0c050c6e945410400381e8cd70450b
Link <2454546080>
2021-01-26 18:35:29 +0800

    add single

--- Tree/PrintABC.go
+++ Tree/PrintABC.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var one= make(chan int,1)
+var two = make(chan int,1)
+var group sync.WaitGroup
+
+//可以使用chan代替group
+var end = make(chan int,1)
+
+func PrintA()  {
+	//defer group.Done()
+	//两个chan控制打印
+	for i:=0;i<5;i++{
+		<-one
+		fmt.Println("A")
+		two<-1
+	}
+}
+
+func PrIntB()  {
+	//defer group.Done()
+	for i:=0;i<5;i++{
+		<-two
+		fmt.Println("B")
+		one<-1
+	}
+	end<-1
+}
+
+func Three(c1 chan bool,c2 chan bool,str string)  {
+	//value 和 真假
+	if _,ok:=<-c1;ok{
+		fmt.Println(str)
+		c2<-true
+	}
+}
+
+func main()  {
+	//启动信号
+	one<-1
+	//group.Add(2)
+	go PrintA()
+	go PrIntB()
+	//group.Wait()
+	<-end
+}
--- design pattern/single.go
+++ design pattern/single.go
@@ -1,1 +1,67 @@
 package design_pattern
+
+import "sync"
+
+//需要的类
+type Link struct {
+	value int
+}
+
+var link *Link
+
+//未加锁懒汉模式
+func GetInstance()*Link  {
+	if link==nil{
+		link = new(Link)
+	}
+	return link
+}
+
+
+//加锁，为了线程安全
+var lock sync.Mutex
+//线程安全的懒汉模式
+func GetInstanceSync()*Link{
+	defer lock.Unlock()
+	lock.Lock()
+	if link==nil{
+		link = new(Link)
+	}
+	return link
+}
+
+
+var Mipha Link
+//饿汉式,直接建立实体，浪费内存
+func GetInstanceStandard() *Link{
+	return &Mipha
+}
+
+
+//双重检查
+func GetTwoSelect()*Link {
+	if link==nil{
+		lock.Lock()
+
+		//再判断
+		if link==nil{
+			link  = new(Link)
+		}
+
+		lock.Unlock()
+	}
+	return link
+}
+
+var once sync.Once
+//once方式建立
+func GetOnce() *Link {
+	once.Do(func() {
+		link  = new(Link)
+	})
+	return link
+}
+
+func main()  {
+
+}
